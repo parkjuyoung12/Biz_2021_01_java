ccdbaac88c053af596a4e0111fd40d434538a88e
parkjuyoung12 <zhseltm@naver.com>
2021-02-04 16:33:35 +0900

    반복문

---
+++ Java_006/src/com/callor/varriabel/Loop_05.java
@@ -0,0 +1,26 @@
+package com.callor.varriabel;
+
+public class Loop_05 {
+	
+	public static void main(String[] args) {
+	
+		int intDan = 7;
+		for(int num = 1; num < 10; num += 1) {
+			
+		}
+		for(int num = 1 ; num < 100; num += 1) {
+			
+		}
+		for(int num = 20; num < 42; num += 1) {
+			System.out.println("안녕");
+			
+		}
+		//  시작하는 값   비교하는값  증가하는값
+		for(int num = 12; num <= 20 ; num += 1) {
+			System.out.println("반가워");
+		}
+		for(int num = 12; num <= 20; num += 2) {
+			System.out.println(num);
+		}
+	}
+}
