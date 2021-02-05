91864ca41f261124e2dba0fafb721291b2ea6e24
parkjuyoung12 <zhseltm@naver.com>
2021-02-05 16:42:21 +0900

    while

--- Java_007/src/com/callor/start/logic/Logic_01.java
+++ Java_007/src/com/callor/start/logic/Logic_01.java
@@ -5,7 +5,7 @@ package com.callor.start.logic;
  * 참(yes, true), 거짓(no, false)를 판단하여
  * 어떤 행위를 수행하는 연산
  */
-public class Logic_01 {
+/* public class Logic_01 {
 	
 	public static void main(String[] args) {
 		// 3 이 3 미만이냐(보다 작냐)
@@ -22,4 +22,4 @@ public class Logic_01 {
 		System.out.println( 3 >= 3);
 	}
 
-}
+}*/
