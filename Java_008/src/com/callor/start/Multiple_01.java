899fffee32774fdb4e1869aa3db01b8b01fba278
parkjuyoung12 <zhseltm@naver.com>
2021-02-08 13:26:25 +0900

    SumOdd

---
+++ Java_008/src/com/callor/start/Multiple_01.java
@@ -0,0 +1,49 @@
+package com.callor.start;
+
+public class Multiple_01 {
+	public static void main(String[] args) {
+
+		int sum = 0;
+		for (int i = 0; i < 100; i++) {
+			if ((i+7) % 3 == 0) {
+			
+				sum += i+7;
+
+				System.out.println("3의 배수 : " + (i+7) + " 입니다.");
+			}
+		}
+		System.out.println("3의 배수의 합계 : " + sum + " 입니다.");
+		
+		System.out.println("");
+		// 2번째 방법
+
+		System.out.println("--------- 절취선 ---------");
+		System.out.println(""); 
+		sum = 0;
+		for (int i = 0; i < 100; i++) {
+			int num = i + 7;
+			if (num % 3 == 0) {
+			
+				sum += num;
+
+				System.out.println("3의 배수 : " + num + " 입니다.");
+			}
+		}
+		System.out.println("3의 배수의 합계 : " + sum + " 입니다.");
+
+		
+		int intSumOdd = 0;
+		for(int i = 0; i <100; i++) {
+			int num = i + 1;
+			boolean bYes = num % 2 == 0;
+			
+			// if(bYes == false) {
+			
+			// bYes 가 true 가 아니면
+			if(!bYes) {
+				intSumOdd += num;
+			}
+		}
+		System.out.println("홀수의 합 : " + intSumOdd);
+	}
+}
---
+++ Java_008/src/com/callor/start/Odd_Even_01.java
@@ -0,0 +1,47 @@
+package com.callor.start;
+
+public class Odd_Even_01 {
+
+	public static void main(String[] args) {
+
+		int sumeve = 0;
+		int sumword = 0;
+
+		for (int num = 0; num < 101; num++) {
+			if (num % 2 == 0) {
+				sumeve += num;
+			}
+
+		}
+		System.out.println("짝수의 합 : " + sumeve);
+
+		for (int num = 0; num < 101; num++) {
+
+			if (num % 2 == 1) {
+				sumword += num;
+			}
+		}
+		System.out.println("홀수의 합 : " + sumword);
+
+		
+		System.out.println("-----------------------------");
+		int sum = 0;
+		for (int i = 0; i < 100; i++) {
+			boolean bYes = i % 2 == 0;
+			if (bYes) {
+				sum += i+2;
+			}
+		}
+		System.out.println("짝수의 값 : " + sum);
+		
+		
+		sum = 0;
+		for (int i = 0; i < 100; i++) {
+			boolean bYes = i % 2 == 1;
+			if (bYes) {
+				sum += i;
+			}
+		}
+		System.out.println("홀수의 값 : " + sum);
+	}
+}
